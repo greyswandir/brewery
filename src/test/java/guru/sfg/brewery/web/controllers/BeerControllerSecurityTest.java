8944cbf513c064970344e497c5409df539e9bc13
OlegVitko <oleg.vitko@gmail.com>
2023-04-04 18:53:51 +0300

    Lesson 21: Test basic auth

--- src/test/java/guru/sfg/brewery/web/controllers/BeerControllerSecurityTest.java
+++ src/test/java/guru/sfg/brewery/web/controllers/BeerControllerSecurityTest.java
@@ -1,20 +1,3 @@
-/*
- *  Copyright 2020 the original author or authors.
- *
- * This program is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
- *
- *     This program is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
- *
- *     You should have received a copy of the GNU General Public License
- *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
-
 package guru.sfg.brewery.web.controllers;
 
 import guru.sfg.brewery.repositories.BeerInventoryRepository;
@@ -25,12 +8,16 @@ import guru.sfg.brewery.services.BreweryService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -58,16 +45,39 @@ class BeerControllerSecurityTest {
     @MockBean
     BeerService beerService;
 
+    @Value("${spring.security.user.name}")
+    public String login;
+
+    @Value("${spring.security.user.password}")
+    public String password;
+
     @BeforeEach
     void setUp() {
         mockMvc = MockMvcBuilders
                 .webAppContextSetup(wac)
+                .apply(springSecurity())
                 .build();
     }
 
+
+    /**
+     * Заглушает авторизацию, навязанную строкой ".apply(springSecurity())"
+     */
+    @WithMockUser
     @Test
     void findBeers() throws Exception {
         mockMvc.perform(get("/beers/find"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("beers/findBeers"))
+                .andExpect(model().attributeExists("beer"));
+    }
+
+    /**
+     * Проводит реальную авторизацию с доступами указанными в application.properties
+     */
+    @Test
+    void findBeersWithBasicAuth() throws Exception {
+        mockMvc.perform(get("/beers/find").with(httpBasic(login, password)))
                 .andExpect(status().isOk())
                 .andExpect(view().name("beers/findBeers"))
                 .andExpect(model().attributeExists("beer"));
