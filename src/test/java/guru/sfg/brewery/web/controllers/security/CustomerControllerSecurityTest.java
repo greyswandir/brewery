c435c9b981cab331fa8cd9957e8aea342aff2686
OlegVitko <oleg.vitko@gmail.com>
2023-04-20 09:32:54 +0300

    Lesson 64: User Roles - using spring expressions

--- src/test/java/guru/sfg/brewery/web/controllers/security/CustomerControllerSecurityTest.java
+++ src/test/java/guru/sfg/brewery/web/controllers/security/CustomerControllerSecurityTest.java
@@ -1,13 +1,17 @@
 package guru.sfg.brewery.web.controllers.security;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 
 import static guru.sfg.brewery.config.SecurityConfig.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -32,5 +36,36 @@ public class CustomerControllerSecurityTest extends BaseSecurity {
     void testListCustomersNoLogIn() throws Exception {
         mockMvc.perform(get("/customers"))
                 .andExpect(status().isUnauthorized());
+    }
+
+    @DisplayName("Add Customers")
+    @Nested
+    class AddCustomers {
+
+        @Rollback
+        @Test
+        void processCreationForm() throws Exception{
+            mockMvc.perform(post("/customers/new")
+                            .param("customerName", "Foo Customer")
+                            .with(httpBasic(ADMIN_USER_SPRING, ADMIN_PASS_TEST)))
+                    .andExpect(status().is3xxRedirection());
+        }
+
+        @Rollback
+        @ParameterizedTest(name = "#{index} with [{arguments}]")
+        @MethodSource("guru.sfg.brewery.web.controllers.security.BaseSecurity#getStreamNotAdmin")
+        void processCreationFormNOTAUTH(String user, String pwd) throws Exception{
+            mockMvc.perform(post("/customers/new")
+                            .param("customerName", "Foo Customer2")
+                            .with(httpBasic(user, pwd)))
+                    .andExpect(status().isForbidden());
+        }
+
+        @Test
+        void processCreationFormNOAUTH() throws Exception{
+            mockMvc.perform(post("/customers/new")
+                            .param("customerName", "Foo Customer"))
+                    .andExpect(status().isUnauthorized());
+        }
     }
 }
