0b07bc6205d7708b65c8250a7744e581cb086663
OlegVitko <oleg.vitko@gmail.com>
2023-09-16 20:43:20 +0300

    Lesson 105: lock user account on failed login attempts

--- src/main/java/guru/sfg/brewery/security/listeners/AuthenticationListener.java
+++ src/main/java/guru/sfg/brewery/security/listeners/AuthenticationListener.java
@@ -1,8 +1,11 @@
 package guru.sfg.brewery.security.listeners;
 
+import guru.sfg.brewery.domain.security.LoginFailure;
 import guru.sfg.brewery.domain.security.LoginSuccess;
 import guru.sfg.brewery.domain.security.User;
+import guru.sfg.brewery.repositories.security.LoginFailureRepo;
 import guru.sfg.brewery.repositories.security.LoginSuccessRepo;
+import guru.sfg.brewery.repositories.security.UserRepo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
@@ -12,12 +15,20 @@ import org.springframework.security.authentication.event.AuthenticationSuccessEv
 import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Component;
 
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Slf4j
 @RequiredArgsConstructor
 @Component
 public class AuthenticationListener {
 
+    private final LoginFailureRepo loginFailureRepo;
+
     private final LoginSuccessRepo loginSuccessRepo;
+
+    private final UserRepo userRepo;
 
     @EventListener
     public void listen(AuthenticationSuccessEvent event) {
@@ -51,6 +62,28 @@ public class AuthenticationListener {
     public void listen(AuthenticationFailureBadCredentialsEvent event) {
         UsernamePasswordAuthenticationToken token = (UsernamePasswordAuthenticationToken) event.getSource();
 
-        log.debug("Following user failed to log in: " + token.getPrincipal());
+        String username = (String) token.getPrincipal();
+        log.debug("Following user failed to log in: " + username);
+
+        LoginFailure.LoginFailureBuilder builder = LoginFailure.builder();
+
+        builder.username(username);
+        userRepo.findByUsername(username).ifPresent(builder::user);
+
+        LoginFailure failure = loginFailureRepo.save(builder.build());
+
+        if (failure.getUser() != null) {
+            lockUserAccount(failure.getUser());
+        }
+    }
+
+    private void lockUserAccount(User user) {
+        List<LoginFailure> failures = loginFailureRepo.findAllByUserAndCreatedDateAfter(user, Timestamp.valueOf(LocalDateTime.now().minusDays(1)));
+
+        if (failures.size() > 3) {
+            log.debug("Locking User Account ...");
+            user.setAccountNonLocked(false);
+            userRepo.save(user);
+        }
     }
 }
