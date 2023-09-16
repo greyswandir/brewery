f81c864d616a9857ff23ccff2e35a1582b1d69a6
OlegVitko <oleg.vitko@gmail.com>
2023-09-16 19:01:10 +0300

    Lesson 103: store auth success into database

--- src/main/java/guru/sfg/brewery/security/listeners/AuthenticationListener.java
+++ src/main/java/guru/sfg/brewery/security/listeners/AuthenticationListener.java
@@ -1,6 +1,9 @@
 package guru.sfg.brewery.security.listeners;
 
+import guru.sfg.brewery.domain.security.LoginSuccess;
 import guru.sfg.brewery.domain.security.User;
+import guru.sfg.brewery.repositories.security.LoginSuccessRepo;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -10,27 +13,37 @@ import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Component;
 
 @Slf4j
+@RequiredArgsConstructor
 @Component
 public class AuthenticationListener {
+
+    private final LoginSuccessRepo loginSuccessRepo;
 
     @EventListener
     public void listen(AuthenticationSuccessEvent event) {
         log.debug("User has been successfully logged in");
 
         if (event.getSource() instanceof UsernamePasswordAuthenticationToken) {
+            LoginSuccess.LoginSuccessBuilder builder = LoginSuccess.builder();
+
             UsernamePasswordAuthenticationToken token = (UsernamePasswordAuthenticationToken) event.getSource();
 
             if (token.getPrincipal() instanceof User) {
                 User user = (User) token.getPrincipal();
+                builder.user(user);
 
                 log.debug("User name logged in: " + user.getUsername());
             }
 
             if (token.getDetails() instanceof WebAuthenticationDetails) {
                 WebAuthenticationDetails details = (WebAuthenticationDetails) token.getDetails();
+                builder.sourceIp(details.getRemoteAddress());
 
                 log.debug("Source IP: " + details.getRemoteAddress());
             }
+
+            LoginSuccess loginSuccess = loginSuccessRepo.save(builder.build());
+            log.debug("Login success saved. Id: " + loginSuccess.getId());
         }
     }
 
