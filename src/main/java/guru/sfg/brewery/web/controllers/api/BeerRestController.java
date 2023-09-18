38aed9fb8b34c1c781404bef0d42fba91ebb16ab
OlegVitko <oleg.vitko@gmail.com>
2023-09-18 09:23:51 +0300

    Lesson 125: CORS by Spring MVC using Configuration and @Cors (deprecated) annotation

--- src/main/java/guru/sfg/brewery/web/controllers/api/BeerRestController.java
+++ src/main/java/guru/sfg/brewery/web/controllers/api/BeerRestController.java
@@ -117,6 +117,7 @@ public class BeerRestController {
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
+    @CrossOrigin
     @PreAuthorize("hasAuthority('beer.delete')")
     @DeleteMapping({"beer/{beerId}"})
     @ResponseStatus(HttpStatus.NO_CONTENT)
